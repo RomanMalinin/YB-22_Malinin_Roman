ace35ab1bd438b513c7208a5c218050f95efec29
RomanMalinin <98349496+RomanMalinin@users.noreply.github.com>
2022-12-01 18:44:53 +0300

    Update Praktika 11.py

--- Praktika 11.py
+++ Praktika 11.py
@@ -5,30 +5,30 @@ from tkinter import *
 def pars():
     username = us.get()
     repositoriy = re.get()
-    r = requests.get(f'https://api.github.com/repos/{username}/{repositoriy}').json()
+    req = requests.get(f'https://api.github.com/repos/{username}/{repositoriy}').json()
     with open('C:\\Users\\malin\\PycharmProjects\\PY\\venv\\pr11', 'a+') as f:
-        if 'company' in r:
-            f.write(f"'company': '{r['company']}\n")
+        if 'company' in req:
+            f.write(f"'company': '{req['company']}\n")
         else:
             f.write("'company: None'\n")
         if 'created_at' in r:
-            f.write(f"'created_at': '{r['created_at']}\n")
+            f.write(f"'created_at': '{req['created_at']}\n")
         else:
             f.write("'None'\n")
-        if 'email' in r:
-            f.write(f"'email': '{r['email']}\n")
+        if 'email' in req:
+            f.write(f"'email': '{req['email']}\n")
         else:
             f.write("'email: None'\n")
-        if 'id' in r:
-            f.write(f"'id': '{r['id']}\n")
+        if 'id' in req:
+            f.write(f"'id': '{req['id']}\n")
         else:
             f.write("'None'\n")
-        if 'name' in r:
-            f.write(f"'name': '{r['name']}\n")
+        if 'name' in req:
+            f.write(f"'name': '{req['name']}\n")
         else:
             f.write("'None'\n")
-        if 'url' in r:
-            f.write(f"'url': '{r['url']}\n")
+        if 'url' in req:
+            f.write(f"'url': '{req['url']}\n")
         else:
             f.write("'None'\n")
 
@@ -46,4 +46,4 @@ us.grid(column=1, row=0)
 re = Entry(window, width=25)
 re.grid(column=1, row=4)
 window.mainloop()
-print (r)
+print (req)
